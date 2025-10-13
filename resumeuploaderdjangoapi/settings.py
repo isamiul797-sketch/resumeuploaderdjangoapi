607ade0215497904b215e60f1c9dce687aad6600
Samiul Islam Sami <isamiul797@gmail.com>
2025-10-13 21:02:55 +0600

    new files added

--- resumeuploaderdjangoapi/settings.py
+++ resumeuploaderdjangoapi/settings.py
@@ -110,3 +110,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+MEDIA_ROOT = BASE_DIR / 'media'
